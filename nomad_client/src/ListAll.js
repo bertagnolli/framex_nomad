713c7dbc93603e87c06452fdffc95fc4d675cf8f
Bertagnolli <carlosb@live.com>
2022-08-16 15:54:04 +0100

    Changed HOST to nomad_app.local to abstract Nomad's IP address in the network

--- nomad_client/src/ListAll.js
+++ nomad_client/src/ListAll.js
@@ -60,7 +60,7 @@ import { PhantomWalletAdapter } from '@solana/wallet-adapter-phantom';
         var nftMetadata = selectedImg
         console.log(nftMetadata);
 
-        const HOST = 'http://127.0.0.1' //'http://192.168.1.135:1243/nomad_app'
+        const HOST = 'http://nomad_app.local' //'http://192.168.1.135:1243/nomad_app'
         const PORT = '1243'
         const ENDPOINT = `${HOST}:${PORT}/api/nomad_app`
 
